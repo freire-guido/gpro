da0c11f1ea14c6ce1dd002b424ed44104a8e07f6
freire-guido <14301656+freire-guido@users.noreply.github.com>
2023-12-09 20:07:23 -0300

    scrape finale

--- update.py
+++ update.py
@@ -1,18 +1,19 @@
+from bs4 import BeautifulSoup
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 import json
 import pandas as pd
-import sys
+import sys, os
 import pickle
-
-with open('data/config.json') as config:
-    config = json.load(config)
 
 season = int(sys.argv[1])
 fromRace = int(sys.argv[2])
 toRace = int(sys.argv[3])
+with open('data/config.json') as config:
+    config = json.load(config)
 
 driver = webdriver.Firefox()
 driver.implicitly_wait(1)
@@ -22,6 +23,8 @@ while driver.current_url != 'https://www.gpro.net/gb/RaceAnalysis.asp':
 
 for race in range(fromRace, toRace + 1):
     driver.get(f'https://www.gpro.net/gb/RaceAnalysis.asp?SR={season},{race}')
-    pd.read_html(driver.page_source)
-    print(pd)
-
+    soup_tables = BeautifulSoup(driver.page_source).find_all('table')
+    for table in config['tables']:
+        dir = f'data/{season}/{race}_{table}'
+        os.makedirs(os.path.dirname(dir), exist_ok = True)
+        pd.read_html(str(soup_tables[table]))[0].to_pickle(dir)
