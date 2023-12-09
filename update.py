a05d0f58ba4eba3c345e2416665aa0e0fdeb0943
freire-guido <14301656+freire-guido@users.noreply.github.com>
2023-12-09 20:03:42 -0300

    proof of concept

--- update.py
+++ update.py
@@ -1,16 +1,27 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+
 import json
+import pandas as pd
+import sys
+import pickle
 
 with open('data/config.json') as config:
     config = json.load(config)
 
+season = int(sys.argv[1])
+fromRace = int(sys.argv[2])
+toRace = int(sys.argv[3])
+
 driver = webdriver.Firefox()
 driver.implicitly_wait(1)
+driver.get('https://www.gpro.net/gb/RaceAnalysis.asp')
+while driver.current_url != 'https://www.gpro.net/gb/RaceAnalysis.asp':
+    pass
 
-driver.get('https://www.gpro.net/gb/RaceAnalysis.asp?')
-driver.find_element(By.NAME, 'textLogin').send_keys(config['username'])
-driver.find_element(By.NAME, 'textPassword').send_keys(config['password'])
-print('tf')
-driver.find_element(By.NAME, 'LogonFake').click()
+for race in range(fromRace, toRace + 1):
+    driver.get(f'https://www.gpro.net/gb/RaceAnalysis.asp?SR={season},{race}')
+    pd.read_html(driver.page_source)
+    print(pd)
+
