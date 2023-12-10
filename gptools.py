088b9d48e365816d479cda094081e28a42055b25
freire-guido <14301656+freire-guido@users.noreply.github.com>
2023-12-10 20:47:22 -0300

    main.py

---
+++ gptools.py
@@ -0,0 +1,43 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+import pandas as pd
+import os
+
+def wait_login(driver):
+    driver.implicitly_wait(1)
+    driver.get('https://www.gpro.net/gb/RaceAnalysis.asp')
+    while driver.current_url != 'https://www.gpro.net/gb/RaceAnalysis.asp':
+        pass
+    return True
+
+def update_data(driver, season, race, tables):
+    driver.get(f'https://www.gpro.net/gb/RaceAnalysis.asp?SR={season},{race}')
+    soup_tables = BeautifulSoup(driver.page_source, "lxml").find_all('table')
+    for name, index in tables.items():
+        dir = f'data/{season}/{race}_{name}'
+        os.makedirs(os.path.dirname(dir), exist_ok = True)
+        pd.read_html(str(soup_tables[index]))[0].to_pickle(dir)
+
+def set_header(df, row):
+    df.columns = df.iloc[row]
+    df.drop(df.index[:(row+1)], inplace = True)
+    return df
+
+def merge_data(season, tables):
+    for table in tables:
+        if table == 'practice':
+            header = 1
+        elif table == 'setup':
+            header = 0
+        else:
+            header = None
+        races = [file.split('_')[0] for file in os.listdir(f'data/{season}') if file.split('_')[1] == 'laps']
+        if header != None:
+            dfs = [set_header(pd.read_pickle(f'data/{season}/{file}'), header)
+                for file in os.listdir(f'data/{season}') if file.split('_')[1] == table and file.split('_')[0] != 'merge']
+        else:
+            dfs = [pd.read_pickle(f'data/{season}/{file}')
+                for file in os.listdir(f'data/{season}') if file.split('_')[1] == table and file.split('_')[0] != 'merge']
+            
+        pd.concat(dfs, keys = races).to_pickle(f'data/{season}/merge_{table}')
---
+++ main.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+import gptools as gp
+import json, sys
+
+season = int(sys.argv[1])
+fromRace = int(sys.argv[2])
+toRace = int(sys.argv[3])
+with open('data/config.json') as config:
+    config = json.load(config)
+
+with webdriver.Firefox() as driver:
+    gp.wait_login(driver)
+    for race in range(fromRace, toRace + 1):
+        gp.update_data(driver, season, race, config['tables'])
+    if config['merge']:
+        gp.merge_data(season, config['tables'])
